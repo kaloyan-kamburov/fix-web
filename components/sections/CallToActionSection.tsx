8ff43032a8225cd19ab360b7b7918fd39e0ae5b4
Kaloyan Kamburov <kamburov.kaloyan@gmail.com>
2025-10-16 15:36:06 +0300

    wip

--- components/sections/CallToActionSection.tsx
+++ components/sections/CallToActionSection.tsx
@@ -8,6 +8,8 @@ import { useTranslations, useLocale } from "next-intl";
 
 export const CallToActionSection = () => {
   const t = useTranslations();
+  const locale = useLocale();
+
   return (
     <section className=" pb-0 px-0 flex flex-col w-full items-center gap-20 relative bg-gray-100 shadow-[0px_4px_4px_#00000040] pt-[16px]  lg:pt-[100px]">
       <div className="flex h-auto lg:h-[342px] items-end justify-center gap-0 lg:gap-[38px] pl-[16px] lg:pl-0 pr-[16px] lg:pr-0 lg:pr-5 py-0 relative w-full bg-[linear-gradient(90deg,rgba(28,28,29,0)_0%,rgba(14,13,13,1)_100%)] flex-col lg:flex-row">
@@ -70,7 +72,7 @@ export const CallToActionSection = () => {
 
               <div className="hidden lg:inline-flex items-start justify-center gap-4 flex-col relative  ">
                 <Link
-                  href="#contact"
+                  href={`/${locale}/categories`}
                   className="h-auto inline-flex items-center justify-center gap-2 px-6 py-3 relative flex-[0_0_auto] rounded-lg border border-solid border-gray-40 shadow-[0px_0px_12px_#00000040] bg-transparent hover:border-accentaccent hover:text-accentaccent transition-all duration-300"
                 >
                   <span className="relative w-fit mt-[-1.00px] font-button font-[number:var(--button-font-weight)]  text-[length:var(--button-font-size)] text-center tracking-[var(--button-letter-spacing)] leading-[var(--button-line-height)] [font-style:var(--button-font-style)] whitespace-nowrap">
@@ -133,7 +135,7 @@ export const CallToActionSection = () => {
             </Link>
           </div>
           <Link
-            href="#contact"
+            href={`/${locale}/categories`}
             className="h-auto inline-flex items-center justify-center gap-2 px-6 py-3 relative flex-[0_0_auto] rounded-lg border border-solid border-gray-40 shadow-[0px_0px_12px_#00000040] bg-transparent hover:border-accentaccent hover:text-accentaccent transition-all duration-300"
           >
             <span className="relative w-fit mt-[-1.00px] font-button font-[number:var(--button-font-weight)] text-gray-00 text-[length:var(--button-font-size)] text-center tracking-[var(--button-letter-spacing)] leading-[var(--button-line-height)] [font-style:var(--button-font-style)]">
